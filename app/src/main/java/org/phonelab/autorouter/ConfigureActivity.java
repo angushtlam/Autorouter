36bbbc46710688b2068b3416edd6cbfdacc6fc9d
Angus Lam <angushtlam@gmail.com>
2016-07-15 22:39:08 -0400

    Moved autorouter stuff into a manager class

--- app/src/main/java/org/phonelab/autorouter/ConfigureActivity.java
+++ app/src/main/java/org/phonelab/autorouter/ConfigureActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import org.phonelab.autorouter.web.AutorouterManager;
 import org.phonelab.autorouter.web.AutorouterWebViewClient;
 
 public class ConfigureActivity extends AppCompatActivity {
@@ -15,8 +16,8 @@ public class ConfigureActivity extends AppCompatActivity {
 
         // Set up WebView
         WebView webView = (WebView) findViewById(R.id.webView);
-        webView.setWebViewClient(new AutorouterWebViewClient()); // Handles HTTP-Authentication that most routers implement
-        webView.getSettings().setJavaScriptEnabled(true); // Enable Javascript for webpage manipulation.
-        webView.loadUrl("http://192.168.1.1");
+        AutorouterManager mng = new AutorouterManager(webView);
+        mng.init();
+        mng.start();
     }
 }
